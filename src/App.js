e3018472e5d12b3bfe7b9d29ed11428bfec1a606
Alon Rubin <alonrubin2@gmail.com>
2021-01-15 12:09:33 +0200

    all done

--- src/App.js
+++ src/App.js
@@ -1,17 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-class App extends React.Component {
+function App() {
 
-  render() {
-    return (
-        <div className="App">
-          <h1>Select an album:</h1>
+  const [albums, setAlbums] = useState([]);
+  const [pics, setPics] = useState([]);
+  const [value, setValue] = useState('');
 
-        </div>
-    );
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/albums')
+      .then(res => res.json())
+      .then(res => setAlbums([...res]))
+
+  }, []);
+
+  function createPic(event) {
+    const albumId = event.target.selectedIndex + 1;
+    setValue(event.target.value)
+    fetch(`https://jsonplaceholder.typicode.com/photos?albumId=${albumId}`)
+      .then(res => res.json())
+      .then(res => setPics([...res]))
   }
 
+
+  return (
+    <div className="App">
+      <h1>Select an album:</h1>
+      <select className="albums" onChange={(event) => createPic(event)}>
+        {albums.map((album, index) => {
+          return <option key={index} value={album.title}>{album.title}</option>
+        })}
+      </select>
+          <div>
+            {pics.map((pic, index) => {
+              return <img key={index} src={pic.thumbnailUrl} alt={`pic no. ${pic.id}`} />
+            })}
+          </div>
+    </div>
+  );
 }
+
+
 
 export default App;
